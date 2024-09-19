6139d4c6523a122ab92fa10781bc29ad892bba70
Noe <8528445+NoeRGuerra@users.noreply.github.com>
2024-09-19 07:34:23 -0400

    Initial commit
    - Initialize as Node package
    - Add webpack configuration
    - Add boilerplate code
    - Add ESLint and Prettier configuration
    - Add Airbnb-base configuration for ESLint

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # WeatherWebApp
+
 WebApp for The Odin Project Weather App Project
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            title: 'Weather App',
+            filename: 'index.html',
+            inject: 'body',
+        }),
+    ],
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: 'defaults' }],
+                        ],
+                    },
+                },
+            },
+        ],
+    },
+};
