984cd66d9d9d9c8d44be6099d37f75b0d52959ef
Noe <8528445+NoeRGuerra@users.noreply.github.com>
2024-09-23 09:55:39 -0400

    Display more current weather data & add styling
    - Remove `console.log` debugging statement for `getWeatherData()`
    - Change tagline element in input page into a label for form
      accesibility
    - Add ID to input field
    - Add optional `location` parameter to `showInputPage()` to facilitate
      debugging of weather display page
    - Move weather data container creation logic from `showWweatherData()`
      into its own function for clarity
    - Refactor return statement from `processWeatherData()` to include only
      relevant values for `weatherObject`
    - Add styling and classnames for weather data containers
    - Add placeholder icon to facilitate preview of weather data elements

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import config from '../config';
+import placeholderIcon from './icons/placeholder.svg';
+import './style.css';
 
 function getWeatherData(location) {
     const { apiKey } = config;
@@ -21,43 +23,115 @@ function getWeatherData(location) {
 function processWeatherData(weatherData) {
     return {
         location: weatherData.resolvedAddress,
-        currentConditions: weatherData.currentConditions,
-        todayData: weatherData.days[0],
+        currentConditions: {
+            conditions: weatherData.currentConditions.conditions,
+            icon: weatherData.currentConditions.icon,
+            temp: weatherData.currentConditions.temp,
+            windSpeed: weatherData.currentConditions.windspeed,
+            chanceOfPrecip: weatherData.currentConditions.precipprob,
+            precipType: weatherData.currentConditions.preciptype,
+        },
+        todayData: {
+            sunrise: weatherData.days[0].sunrise,
+            sunset: weatherData.days[0].sunset,
+            maxTemp: weatherData.days[0].tempmax,
+            minTemp: weatherData.days[0].tempmin,
+        },
+        todayHourly: weatherData.days[0].hours,
         twoWeeks: weatherData.days.slice(1),
     };
 }
 
+function createDataContainer(value, text) {
+    const container = document.createElement('div');
+    const valueText = document.createElement('p');
+    const valueLabel = document.createElement('p');
+
+    valueText.textContent = value;
+    valueLabel.textContent = text;
+
+    container.append(valueText, valueLabel);
+    return container;
+}
+
+function createTodayWeatherContainer(weatherObject) {
+    const container = document.createElement('div');
+    container.className = 'current-weather';
+    const leftContainer = document.createElement('div');
+    leftContainer.className = 'top-left';
+    const rightContainer = document.createElement('div');
+    rightContainer.className = 'top-right';
+
+    const placeholderImg = document.createElement('img');
+    placeholderImg.src = placeholderIcon;
+    placeholderImg.style.width = '100px';
+    placeholderImg.style.height = 'auto';
+    const currentTempText = document.createElement('p');
+    currentTempText.textContent = `${weatherObject.currentConditions.temp} F`;
+    const conditionsText = document.createElement('p');
+    conditionsText.textContent = weatherObject.currentConditions.conditions;
+
+    leftContainer.append(placeholderImg, currentTempText, conditionsText);
+
+    const todayWeatherData = {
+        High: weatherObject.todayData.maxTemp,
+        Low: weatherObject.todayData.minTemp,
+        Sunrise: weatherObject.todayData.sunrise,
+        Sunset: weatherObject.todayData.sunset,
+        Rain: weatherObject.currentConditions.chanceOfPrecip,
+        Wind: weatherObject.currentConditions.windSpeed,
+    };
+    const dataArray = [];
+    for (const key in todayWeatherData) {
+        const dataContainer = createDataContainer(todayWeatherData[key], key);
+        dataArray.push(dataContainer);
+    }
+
+    for (const valueContainer of dataArray) {
+        rightContainer.append(valueContainer);
+    }
+
+    container.append(leftContainer, rightContainer);
+
+    return container;
+}
+
 function showWeatherData(location) {
-    const container = document.querySelector('#content');
-    container.replaceChildren();
+    const mainContainer = document.querySelector('#content');
+    mainContainer.replaceChildren();
     const loadingParagraph = document.createElement('p');
     loadingParagraph.textContent = 'Loading...';
-    container.append(loadingParagraph);
+    mainContainer.append(loadingParagraph);
 
     getWeatherData(location)
         .then((data) => {
             const weatherObj = processWeatherData(data);
-            container.replaceChildren();
-            const locationParagraph = document.createElement('p');
-            const currentTempParagraph = document.createElement('p');
-            locationParagraph.textContent = weatherObj.location;
-            currentTempParagraph.textContent = `${weatherObj.currentConditions.temp} F`;
-            container.append(locationParagraph, currentTempParagraph);
+            console.log(weatherObj);
+            const weatherContainer = createTodayWeatherContainer(weatherObj);
+            mainContainer.replaceChildren();
+            mainContainer.append(weatherContainer);
         })
         .catch((error) => {
-            container.replaceChildren();
+            mainContainer.replaceChildren();
             const errorMessage = document.createElement('p');
             errorMessage.textContent = `Error: ${error.message}`;
-            container.append(errorMessage);
+            mainContainer.append(errorMessage);
         });
 }
 
-function showInputPage() {
+function showInputPage(location = null) {
+    if (location) {
+        showWeatherData(location);
+        return;
+    }
     const container = document.querySelector('#content');
-    const tagline = document.createElement('p');
-    tagline.textContent = 'Enter location:';
     const inputForm = document.createElement('form');
+    const tagline = document.createElement('label');
+    tagline.setAttribute('for', 'location-input');
+    tagline.textContent = 'Enter location:';
     const input = document.createElement('input');
+    input.id = 'location-input';
+    input.type = 'text';
     const submitBtn = document.createElement('button');
     submitBtn.textContent = 'Submit';
     submitBtn.addEventListener('click', (e) => {
@@ -65,10 +139,8 @@ function showInputPage() {
         showWeatherData(input.value);
     });
 
-    inputForm.append(input, submitBtn);
-    container.append(tagline, inputForm);
+    inputForm.append(tagline, input, submitBtn);
+    container.append(inputForm);
 }
-
-getWeatherData('Reynosa').then((data) => console.log(data));
 
 showInputPage();
